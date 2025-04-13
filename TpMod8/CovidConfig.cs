19018a5e1e19083febf5a0f0b79a52653b6ee52c
Alyaras <alyarahmadayanis@gmail.com>
2025-04-14 00:46:55 +0700

    menyelesaikan program dengan runtime configuration

---
+++ TpMod8/CovidConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TpMod8
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_demam { get; set; } = 14;
+
+        public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+
+        public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("D:\\Praktikum Konstruksi PL\\TpMod8\\TpMod8\\covid_config.json");
+            CovidConfig config = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+
+            satuan_suhu = config.satuan_suhu;
+            batas_hari_demam = config.batas_hari_demam;
+            pesan_ditolak = config.pesan_ditolak;
+            pesan_diterima = config.pesan_diterima;
+        }
+
+        public void SaveJSON()
+        {
+            string jsonString = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("D:\\Praktikum Konstruksi PL\\TpMod8\\TpMod8\\covid_config.json", jsonString);
+        }
+        public void UbahSatuan()
+        {
+            satuan_suhu = (satuan_suhu == "celcius") ? "fahrenheit" : "celcius";
+            ReadJSON();
+        }
+
+    }
+}
--- TpMod8/Program.cs
+++ TpMod8/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("pertama");
+using TpMod8;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+        config.UbahSatuan();
+
+        Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai<celcius>");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool kondisiSesuai = false;
+        if (config.satuan_suhu == "celcius")
+        {
+            kondisiSesuai = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            kondisiSesuai = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        if (kondisiSesuai && hari < config.batas_hari_demam)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+
+}
